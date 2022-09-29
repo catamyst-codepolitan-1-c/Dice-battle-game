545cc28bbde0387efa9dd5b7a363f78d0a1ab9ad
zul1996 <mzulnewbie@gmail.com>
2022-09-29 16:46:30 +0700

    fix JavaScript error code

--- app.js
+++ app.js
@@ -12,26 +12,39 @@ function roll() {
   dice.forEach(function (die) {
     die.classList.add("shake");
   });
+  setTimeout(function () {
+    dice.forEach(function (die) {
+      die.classList.remove("shake");
+    });
+
+    let dieOneValue = Math.floor(Math.random() * 5);
+    let dieTwoValue = Math.floor(Math.random() * 5);
+    let dieThreeValue = Math.floor(Math.random() * 5);
+    let dieFourValue = Math.floor(Math.random() * 5);
+    // console.log(diceImg);
+    document
+      .getElementById("dice-01")
+      .setAttribute("src", diceImg[dieOneValue]);
+    document
+      .getElementById("dice-02")
+      .setAttribute("src", diceImg[dieTwoValue]);
+    document
+      .getElementById("dice-03")
+      .setAttribute("src", diceImg[dieThreeValue]);
+    document
+      .getElementById("dice-04")
+      .setAttribute("src", diceImg[dieFourValue]);
+
+    document.getElementById("total").innerHTML =
+      "Your roll is " +
+      (dieOneValue +
+        1 +
+        (dieTwoValue + 1) +
+        1 +
+        (dieThreeValue + 1) +
+        1 +
+        (dieFourValue + 1));
+  }, 1000);
 }
 
-let dieOneValue = Math.floor(Math.random() * 5);
-let dieTwoValue = Math.floor(Math.random() * 5);
-let dieThreeValue = Math.floor(Math.random() * 5);
-let dieFourValue = Math.floor(Math.random() * 5);
-// console.log(diceImg);
-document.querySelector("#dice-01").setAttribute("src", diceImg[dieOneValue]);
-document.querySelector("#dice-02").setAttribute("src", diceImg[dieTwoValue]);
-document.querySelector("#dice-03").setAttribute("src", diceImg[dieThreeValue]);
-document.querySelector("#dice-04").setAttribute("src", diceImg[dieFourValue]);
-
-//     document.querySelector("#total").innerHTML =
-//       "Your roll is " +
-//       (dieOneValue +
-//         1 +
-//         (dieTwoValue + 1) +
-//         1 +
-//         (dieThreeValue + 1) +
-//         1 +
-//         (dieFourValue + 1));
-//   }, 1000);
-// }
+roll();
